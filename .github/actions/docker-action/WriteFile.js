0f0700fe628dbe4563f076b3366b59429db73396
Alvaro Solari <Ads63@Students.UWF.edu>
2021-12-01 11:47:42 -0600

    Added Random Number Generation

---
+++ .github/actions/docker-action/WriteFile.js
@@ -0,0 +1,9 @@
+/**
+ * The following JavaSCript file will generate two random numbers that are between 0 and 99 and write them to a file. 
+ * It will write them to the ...
+ */
+
+let randomOne = Math.random() * 100; 
+let randomTwo = Math.random() * 100; 
+
+console.log(`The numbers generated were: ${randomOne} and ${randomTwo}`); 
