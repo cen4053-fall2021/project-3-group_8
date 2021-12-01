90a99603d06344d20a19835ead6b6382c5739d0a
Alvaro Solari <Ads63@Students.UWF.edu>
2021-12-01 13:44:51 -0600

    Mofied Files

--- .github/actions/docker-action/WriteFile.js
+++ .github/actions/docker-action/WriteFile.js
@@ -2,8 +2,12 @@
  * The following JavaSCript file will generate two random numbers that are between 0 and 99 and write them to a file. 
  * It will write them to the ...
  */
+let finalString = ""; 
 
-let randomOne = Math.random() * 100; 
-let randomTwo = Math.random() * 100; 
+let randomOne = Math.floor(Math.random() * 100); 
+let randomTwo = Math.floor(Math.random() * 100); 
 
-console.log(`The numbers generated were: ${randomOne} and ${randomTwo}`); 
+finalString = randomOne + " " + randomTwo; 
+
+console.log(`The written contents for the file will be: ${finalString}`);
+
